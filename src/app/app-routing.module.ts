0efc2fb5206fb1cb028bf5b25b8c415c2b655004
Ilaria <61754608+Ila-inGit@users.noreply.github.com>
2023-05-24 12:44:34 +0000

    feat(home): First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'search/:searchTerm', component: HomeComponent },
+  { path: 'tag/:tag', component: HomeComponent }
+];
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
